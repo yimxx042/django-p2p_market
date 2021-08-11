4bcadbcd798a21c89e1c97de87ae47947bb25687
yimxx042 <79169462+yimxx042@users.noreply.github.com>
2021-08-11 10:13:43 -0500

    email verification, password reset redirection

--- p2p_market/urls.py
+++ p2p_market/urls.py
@@ -15,9 +15,22 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.views.generic import TemplateView
+from podomarket.views import CustomPasswordChangeView
 
 urlpatterns = [
+    # admin
     path('admin/', admin.site.urls),
+
+    # podomarket
     path('', include('podomarket.urls')),
+
+    # allauth
+    path(
+        'email-confirmation-done/',
+        TemplateView.as_view(template_name='podomarket/email_confirmation_done.html'),
+        name='account_email_confirmation_done',
+    ),
+    path('password/change/', CustomPasswordChangeView.as_view(), name='account_change_password'),
     path('', include('allauth.urls')),
 ]
